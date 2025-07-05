75eaff41629c0b36a4daf52d0016031f1fa8eb0d
PD <praveen240903@gmail.com>
2025-07-05 23:29:42 +0530

    initial commit

---
+++ src/main/java/com/security/securityApplicaton/SecurityApplicatonApplication.java
@@ -0,0 +1,13 @@
+package com.security.securityApplicaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplicatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplicatonApplication.class, args);
+	}
+
+}
